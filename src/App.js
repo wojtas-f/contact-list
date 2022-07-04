951d5dba4b27ae594723ec738fae7b1f66aa3b18
wojtas_f <wojtas_f@outlook.com>
2022-07-04 16:25:27 +0200

    feat: get contacts and render placeholder contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import './App.css';
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap'
 
 import TopBar from './Components/TopBar'
+import SingleContact from './Components/SingleContact'
+
+import getContacts from './Services/getContacts';
 
 function App() {
+  const [contacts, setContacts] = useState([])
+
+  const get = async () => {
+    const res = await getContacts()
+    if (res) setContacts(res)
+  }
+
+  useEffect(() => {
+    get();
+  }, [])
+
   return (
     <Container fluid className='p-0'>
       <TopBar />
+      <Container>
+        {contacts && contacts.map((contact, index) => {
+          return <SingleContact key={`contact_${index}`} />
+        })}
+      </Container>
     </Container>
   );
 }
---
+++ src/Components/SingleContact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleContact = () => {
+    return (
+        <div>SingleContact</div>
+    )
+}
+
+export default SingleContact
