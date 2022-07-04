5b10d533dce90e4e2cd2f3be37bc25ea3fc98b43
wojtas_f <wojtas_f@outlook.com>
2022-07-04 16:13:41 +0200

    feat: add services and getContact request

---
+++ src/Services/config.js
@@ -0,0 +1,9 @@
+
+const getRequestConfig = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+}
+
+export default getRequestConfig 
---
+++ src/Services/getContacts.js
@@ -0,0 +1,10 @@
+import getRequestConfig from './config'
+
+const getContacts = async () => {
+    const URL = "https://teacode-recruitment-challenge.s3.eu-central-1.amazonaws.com/users.json"
+    const response = await fetch(URL, getRequestConfig)
+    if (!response) return false
+    return response.json()
+}
+
+export default getContacts
