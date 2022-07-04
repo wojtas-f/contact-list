9790290413a15dc907cf065c9ffee0e90c2daf31
wojtas_f <wojtas_f@outlook.com>
2022-07-04 16:07:23 +0200

    feat: initial setup

---
+++ src/Components/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const TopBar = () => {
+    return (
+        <Container fluid className='bg-dark text-white p-2 text-center'>
+            <h1>Contacts</h1>
+        </Container>
+    )
+}
+
+export default TopBar
